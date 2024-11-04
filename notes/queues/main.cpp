24108ce2da013fa210f4e2e026579d5e156c9137
Hayden Armstrong <hdarmstrong@mavs.coloradomesa.edu>
2024-11-04 14:14:28 -0700

    queues

--- notes/queues/main.cpp
+++ notes/queues/main.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
-
+#include <cmath> // for abs in size()
 using namespace std;
 
 template <class T1>
-class Queue
-{
-    private:
-    const int maxQueue = 100;
-    T1 _queueArr[];
+class Queue {
+private:
+    static const int maxQueue = 100;
+    T1 _queueArr[maxQueue];
     int _beg, _end;
 
-    public:
+public:
     Queue();
     void enqueue(T1);
     T1 dequeue();
@@ -21,74 +20,99 @@ class Queue
     T1 end();
 };
 
-int main(int argc, char* argv[])
-{
+int main() {
+    Queue<int> myQueue;
+    int inData;
+
+    cout << "Enter -99999 to quit" << endl;
+    while (true) {
+        cout << "Enter a number: ";
+        cin >> inData;
+        if (inData == -99999) break;  // Exit condition added
+        myQueue.enqueue(inData);
+    }
+
+    cout << "Queue Size: " << myQueue.size() << endl;
+    cout <<  "Queue Front: " << myQueue.front() << endl;
+    cout <<  "Queue end: " << myQueue.end() << endl;
+
+    while (myQueue.size() > 0)
+    {
+        cout << "data: " << myQueue.dequeue() << " ";
+    }
     return 0;
 }
 
 template <class T1>
-
-Queue<T1>::Queue()
-{
-    _beg = -1;  
+Queue<T1>::Queue() {
+    _beg = -1;
     _end = -1;
 }
 
 template <class T1>
-void Queue<T1>::enqueue(T1 data)
-{
-
-}
-
-template <class T1>
-T1 Queue<T1>::dequeue()
-{
-
-}
-
-template <class T1>
-bool Queue<T1>::empty()
-{
-    if(_beg == -1 ) return true;
-    return false;
-}
-
-template <class T1>
-bool Queue<T1>::full()
-{
-    //cant just return !empty();
-    if ((_end > _beg && _beg == 0 && _end == maxQueue -1) || (_end - _beg == -1) || !empty)
-    {
-        return true;
+void Queue<T1>::enqueue(T1 data) {
+    if (full()) {
+        cout << "Queue is full" << endl;
+        return;
     }
-
-    return false;
+    if (empty()) {
+        _beg = 0;
+    }
+    _end = (_end + 1) % maxQueue;
+    _queueArr[_end] = data;
 }
 
 template <class T1>
-int Queue<T1>::size()
-{
-    if (empty())
-    {
-        return 0;
+T1 Queue<T1>::dequeue() {
+    if (empty()) {
+        cout << "Queue is empty" << endl;
+        return -1;  // Default return value for empty queue
     }
-    if (_end > _beg)
-    {
+    T1 tmpData = _queueArr[_beg];
+     if (_beg == _end) {  // Queue becomes empty after dequeue
+         _beg = -1;
+         _end = -1;
+     } else 
+     {
+         _beg = (_beg + 1) % maxQueue;
+     }
+    return tmpData;
+}
+
+template <class T1>
+bool Queue<T1>::empty() {
+    return _beg == -1;
+}
+
+template <class T1>
+bool Queue<T1>::full() {
+    return (_end + 1) % maxQueue == _beg;
+}
+
+template <class T1>
+int Queue<T1>::size() {
+    if (empty()) return 0;
+    if (_end >= _beg)
         return _end - _beg + 1;
-    }
-    else{
-        return maxQueue - abs((_end - _beg) + 1)
+    return maxQueue - _beg + _end + 1;
+}
+
+template <class T1>
+T1 Queue<T1>::front() {
+    if (!empty()) {
+        return _queueArr[_beg];
+    } else {
+        cout << "Queue is empty" << endl;
+        return -1;
     }
 }
 
 template <class T1>
-T1 Queue<T1>::front()
-{
-
-}
-
-template <class T1>
-T1 Queue<T1>::end()
-{
-
+T1 Queue<T1>::end() {
+    if (!empty()) {
+        return _queueArr[_end];
+    } else {
+        cout << "Queue is empty" << endl;
+        return -1;
+    }
 }
