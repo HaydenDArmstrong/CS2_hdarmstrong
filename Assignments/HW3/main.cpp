3f3e21781471beb5f74667b303f7893a259191d1
Hayden Armstrong <hdarmstrong@mavs.coloradomesa.edu>
2024-12-06 14:43:44 -0700

    hw3 test case fix

--- Assignments/HW3/main.cpp
+++ Assignments/HW3/main.cpp
@@ -92,17 +92,19 @@ void test()
 
 int main(int argc, char *argv[])
 {
+
+    if(argc >= 2 && (string)argv[1] == "test")
+    {
+        test();
+        return 0;
+    }
    
         string expression;
         cout << "Enter an RPN expression: ";
         getline(cin, expression);
         double result = evaluateRPN(expression);
         cout << expression << " = " << result << endl;
-    if(argc >= 2 && (string)argv[1] == "test")
-    {
-        test();
-        return 0;
-    }
+    
 
     return 0;
 }
